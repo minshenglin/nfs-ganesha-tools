fcbf1da6ceafa6828825c2936727b1b6f0bd3fbe
Vincent <minsheng.l@inwinstack.com>
2018-07-30 03:55:27 +0000

    add update (put) ganesha config method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from ceph import CephHandler
 from ganesha import GaneshaConfig
 
@@ -11,4 +12,17 @@ def get_config():
     ceph_handler = CephHandler()
     content = ceph_handler.read("nfs-ganesha", "export")
     config = GaneshaConfig.parser(content) 
+    return jsonify(config.dict())
+
+@app.route('/api/v1.0/nfs/config', methods=['PUT'])
+def update_confg():
+    if not request.json:
+        abort(400)
+    
+    ceph_handler = CephHandler()
+    if 'export' not in request.get_json():
+        abort(400)
+
+    config = GaneshaConfig.parserJson(request.json)
+    ceph_handler.write("nfs-ganesha", "export", str(config))
     return jsonify(config.dict())
