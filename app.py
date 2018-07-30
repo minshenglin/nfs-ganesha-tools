0343654a8748b520aab276916dc6d0925d21a009
Vincent <minsheng.l@inwinstack.com>
2018-07-30 07:20:51 +0000

    add http basic auth

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import jsonify
 from flask import request
 from ceph import CephHandler
 from ganesha import GaneshaConfig
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
-#app.run(port=8080, debug=True)
+auth = HTTPBasicAuth()
 
 @app.route('/api/v1.0/nfs/config', methods=['GET'])
+@auth.login_required
 def get_config():
     ceph_handler = CephHandler()
     content = ceph_handler.read("nfs-ganesha", "export")
@@ -15,6 +17,7 @@ def get_config():
     return jsonify(config.dict())
 
 @app.route('/api/v1.0/nfs/config', methods=['PUT'])
+@auth.login_required
 def update_confg():
     if not request.json:
         abort(400)
@@ -26,3 +29,19 @@ def update_confg():
     config = GaneshaConfig.parserJson(request.json)
     ceph_handler.write("nfs-ganesha", "export", str(config))
     return jsonify(config.dict())
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+
+    usr = ceph_handler.read("nfs-ganesha", "username")
+    pw = ceph_handler.read("nfs-ganesha", "password")
+
+    return username == usr and password == pw
+
+if __name__ == '__main__':
+    ceph_handler = CephHandler()
+    ceph_handler.write("nfs-ganesha", "username", "admin")
+    ceph_handler.write("nfs-ganesha", "password", "password")
+    app.run(host='192.168.15.100', port=5000, debug=True)
