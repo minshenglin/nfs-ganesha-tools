d35103efa1a17e10c0118bcb46755e59a838a821
Vincent <minsheng.l@inwinstack.com>
2018-07-31 05:47:44 +0000

    add export test case

--- test_ganesha.py
+++ test_ganesha.py
@@ -1,17 +1,40 @@
 import pytest
 import ganesha
 
+# test CephfsFsal
+
+def __get_cephfs_fsal_str():
+    return "Name=CEPH;"
+
+def __get_cephfs_fsal_dict():
+    return {"name": "ceph"}
+
+def __get_cephfs_fsal_obj():
+    return ganesha.CephfsFsal()
+
 def test_cephfs_fsal_cast_from_string():
-    fsal = ganesha.Fsal.parser("Name=CEPH;")
+    s = __get_cephfs_fsal_str()
+    fsal = ganesha.Fsal.parser(s)
     assert isinstance(fsal, ganesha.CephfsFsal)
 
 def test_cephfs_fsal_cast_from_dict():
-    fsal = ganesha.Fsal.parserJson({"name": "ceph"})
+    d = __get_cephfs_fsal_dict()
+    fsal = ganesha.Fsal.parser(d)
     assert isinstance(fsal, ganesha.CephfsFsal)
 
 def test_cephfs_fsal_cast_from_string_to_dict():
-    fsal = ganesha.Fsal.parser("NAME=Ceph;")
-    assert {"name": "ceph"} == fsal.dict()
+    s = __get_cephfs_fsal_str()
+    fsal = ganesha.Fsal.parser(s)
+    d = __get_cephfs_fsal_dict()
+    assert fsal.dict() == d
+
+def test_cephfs_fsal_cast_from_dict_to_string():
+    d = __get_cephfs_fsal_dict()
+    fsal = ganesha.Fsal.parser(d)
+    s = __get_cephfs_fsal_str()
+    assert str(fsal) == s
+
+# test RgwFsal
 
 def __get_rgw_fsal_str():
     return "Name=RGW;User_Id=nfs;Access_Key_Id=30GAEOGMTRX0SKWBAD19;Secret_Access_Key=DGMsovPHztquIllIKDJNVvf931xke97ABLsobpTI;"
@@ -19,25 +42,18 @@ def __get_rgw_fsal_str():
 def __get_rgw_fsal_dict():
     return {"name": "rgw", "user_id": "nfs", "access_key_id": "30GAEOGMTRX0SKWBAD19", "secret_access_key": "DGMsovPHztquIllIKDJNVvf931xke97ABLsobpTI"}
 
-def test_cephfs_fsal_cast_from_dict_to_string():
-    fsal = ganesha.Fsal.parserJson({"name": "ceph"})
-    assert str(fsal) == "Name=CEPH;"
+def __get_rgw_fsal_obj():
+    return ganesha.RgwFsal("nfs", "30GAEOGMTRX0SKWBAD19", "DGMsovPHztquIllIKDJNVvf931xke97ABLsobpTI")
 
 def test_rgw_fsal_cast_from_string():
     s = __get_rgw_fsal_str()
     fsal = ganesha.Fsal.parser(s)
-    assert isinstance(fsal, ganesha.RgwFsal)
-    assert fsal.user_id == "nfs"
-    assert fsal.access_key == "30GAEOGMTRX0SKWBAD19"
-    assert fsal.secret_key == "DGMsovPHztquIllIKDJNVvf931xke97ABLsobpTI"
+    assert fsal == __get_rgw_fsal_obj()
 
 def test_rgw_fsal_cast_from_dict():
     d = __get_rgw_fsal_dict()
-    fsal = ganesha.Fsal.parserJson(d)
-    assert isinstance(fsal, ganesha.RgwFsal)
-    assert fsal.user_id == "nfs"
-    assert fsal.access_key == "30GAEOGMTRX0SKWBAD19"
-    assert fsal.secret_key == "DGMsovPHztquIllIKDJNVvf931xke97ABLsobpTI"
+    fsal = ganesha.Fsal.parser(d)
+    assert fsal == __get_rgw_fsal_obj()
 
 def test_rgw_fsal_cast_from_string_to_dict():
     s = __get_rgw_fsal_str()
@@ -47,6 +63,81 @@ def test_rgw_fsal_cast_from_string_to_dict():
 
 def test_rgw_fsal_cast_from_dict_to_string():
     d = __get_rgw_fsal_dict()
-    fsal = ganesha.Fsal.parserJson(d)
+    fsal = ganesha.Fsal.parser(d)
     s = __get_rgw_fsal_str()
     assert str(fsal) == s
+
+# test Clinet
+
+def __get_client_str():
+    return "Clients=192.168.15.11,192.168.16.0/24;Squash=No_Root_Squash;Access_Type=RW;" 
+
+def __get_client_dict():
+    return {"clients": ["192.168.15.11","192.168.16.0/24"], "access_type": "RW", "squash": "No_Root_Squash"}
+
+def __get_client_obj():
+    return ganesha.Client(["192.168.15.11", "192.168.16.0/24"], 
+           access_type=ganesha.AccessType.RW, 
+           squash=ganesha.Squash.No_Root_Squash)
+
+def test_client_cast_from_string():
+    s = __get_client_str()
+    client = ganesha.Client.parser(s)
+    assert client == __get_client_obj()
+    
+def test_client_cast_from_dict():
+    d = __get_client_dict()
+    client = ganesha.Client.parser(d)
+    assert client == __get_client_obj()
+
+def test_client_cast_from_string_to_dict():
+    s = __get_client_str()
+    client = ganesha.Client.parser(s)
+    d = __get_client_dict()
+    assert d == client.dict()
+
+def test_client_cast_from_dict_to_string():
+    d = __get_client_dict()
+    client = ganesha.Client.parser(d)
+    s = __get_client_str()
+    assert s == str(client)
+
+# test Export
+
+def __get_export_str():
+    client_str = __get_client_str()
+    fsal_str = __get_rgw_fsal_str()
+    return "Export{Export_Id=1234;Path=/test;Pseudo=/nfs/test;Protocols=4;Transports=TCP;" + \
+           "CLIENT{%s}FSAL{%s}}" % (client_str, fsal_str)
+
+def __get_export_dict():
+    client_dict = __get_client_dict()
+    fsal_dict = __get_rgw_fsal_dict()
+    return {"export_id": "1234", "path": "/test", "pseudo": "/nfs/test", "protocols": "4",
+            "transports": "TCP", "client": [client_dict], "fsal": fsal_dict}
+def __get_export_obj():
+    clients = [__get_client_obj()]
+    fsal = __get_rgw_fsal_obj()
+    return ganesha.Export("1234", "/test", clients, fsal, pseudo="/nfs/test", protocols="4", transports="TCP")
+
+def test_export_cast_from_str(): 
+    s = __get_export_str()
+    export = ganesha.Export.parser(s)
+    assert export == __get_export_obj()
+
+def test_export_cast_from_dict(): 
+    d = __get_export_dict()
+    export = ganesha.Export.parser(d)
+    assert export == __get_export_obj()
+
+def test_export_cast_from_string_to_dict():
+    s = __get_export_str()
+    export = ganesha.Export.parser(s)
+    d = __get_export_dict()
+    assert d == export.dict()
+
+def test_export_cast_from_dict_to_string():
+    d = __get_export_dict()
+    export = ganesha.Export.parser(d)
+    s = __get_export_str()
+    assert s == str(export)
